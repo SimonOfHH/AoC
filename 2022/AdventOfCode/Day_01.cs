f2e434320286b72020baf539e97db247ccf40687
SimonOfHH <17087203+SimonOfHH@users.noreply.github.com>
2022-12-01 07:03:25 +0100

    Solve Day 1

--- 2022/AdventOfCode/Day_01.cs
+++ 2022/AdventOfCode/Day_01.cs
@@ -3,13 +3,35 @@
 public class Day_01 : BaseDay
 {
     private readonly string _input;
+    private Dictionary<int, int> elfCalories;
 
     public Day_01()
     {
         _input = File.ReadAllText(InputFilePath);
+        elfCalories = ParseInput();
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
+        return new(elfCalories.MaxBy(kvp => kvp.Value).Value.ToString());
+    }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    public override ValueTask<string> Solve_2()
+    {
+        return new ValueTask<string>(elfCalories.OrderByDescending(kvp => kvp.Value).Take(3).Sum(kvp => kvp.Value).ToString());
+    }
+
+    private Dictionary<int, int> ParseInput()
+    {
+        var values = new Dictionary<int, int>();
+        foreach (var line in File.ReadAllLines(InputFilePath))
+        {
+            if ((String.IsNullOrEmpty(line)) || (values.Count == 0))
+                values.Add(values.Count + 1, 0);
+            int calories = 0;
+            if (int.TryParse(line, out calories))
+                values[values.Count] += calories;
+        }
+        return values;
+    }
 }
