c129fde2404d3eb9ca98a63558139890bd6130bf
SimonOfHH <17087203+SimonOfHH@users.noreply.github.com>
2022-12-01 21:14:26 +0100

    add template to copy paste

---
+++ 2022/AdventOfCode/Day_Template.cs
@@ -0,0 +1,14 @@
+namespace AdventOfCode;
+
+public class Day_XX : BaseDay
+{
+    private readonly string _input;
+
+    public Day_XX()
+    {
+    }
+
+    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+
+    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+}
