ad3980b4f8927e1ce88e91ef2be034ea392f7eed
SimonOfHH <17087203+SimonOfHH@users.noreply.github.com>
2022-12-06 19:50:58 +0100

    Update template

--- 2022/AdventOfCode/Day_Template.cs
+++ 2022/AdventOfCode/Day_Template.cs
@@ -2,11 +2,18 @@ namespace AdventOfCode;
 
 public class Day_XX : BaseDay
 {
+    private readonly bool Sample = false;
     public Day_XX()
     {
+        var input = ParseInput();
     }
 
     public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
 
     public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+
+    private string[] ParseInput()
+    {
+        return this.GetInput(Sample);
+    }
 }
