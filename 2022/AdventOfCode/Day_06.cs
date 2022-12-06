00a540dbe28f5b8aba9900df9d6777a618f1cbbf
SimonOfHH <17087203+SimonOfHH@users.noreply.github.com>
2022-12-06 08:10:01 +0100

    Day 06 refactored, just in case for tomorrow

--- 2022/AdventOfCode/Day_06.cs
+++ 2022/AdventOfCode/Day_06.cs
@@ -2,21 +2,37 @@ namespace AdventOfCode;
 
 public class Day_06 : BaseDay
 {
-    public bool Sample = false;
+    private readonly bool Sample = true;
+    private readonly DataStream dataStream;
     public Day_06()
     {
+        dataStream = ParseInput();
     }
 
-    public override ValueTask<string> Solve_1() => new(ParseInput(4).ToString());
+    public override ValueTask<string> Solve_1() => new(dataStream.StartOfPacket.ToString());
 
-    public override ValueTask<string> Solve_2() => new(ParseInput(14).ToString());
+    public override ValueTask<string> Solve_2() => new(dataStream.StartOfMessage.ToString());
 
-    public int ParseInput(int length)
+    private DataStream ParseInput()
     {
-        string input = this.GetInput(Sample).First();
-        for (int i = 0; i < input.Length; i++)
+        return new DataStream(this.GetInput(Sample).First());
+    }
+}
+
+public class DataStream
+{
+    public string StreamData { get; }
+    public int StartOfPacket => GetStartOfIndicator(4);
+    public int StartOfMessage => GetStartOfIndicator(14);
+    public DataStream(string s)
+    {
+        StreamData = s;
+    }
+    private int GetStartOfIndicator(int length)
+    {
+        for (int i = 0; i < StreamData.Length; i++)
         {
-            if (input.Skip(i).Take(length).Distinct().Count() == length)
+            if (StreamData.Skip(i).Take(length).Distinct().Count() == length)
                 return i + length;
         }
         return 0;
